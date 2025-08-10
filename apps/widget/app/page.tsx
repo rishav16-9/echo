725f0038cd9a9e638f66ed33ec466b835aa35652
Rishav <suranarishav08@gmail.com>
2025-08-10 12:01:23 +0530

    Setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,16 +1,16 @@
-import { add } from "@workspace/math/add"
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+import { add } from "@workspace/math/add";
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World / Web</h1>
+        <h1 className="text-2xl font-bold">Hello World / Widget</h1>
         <Button size="sm">Button</Button>
-        {add(2,2)}
+        {add(2, 2)}
         <Input />
       </div>
     </div>
-  )
+  );
 }

ac0c678e29dc8e9749bcb3dd6b29e76f763ea253
Rishav <suranarishav08@gmail.com>
2025-08-16 12:26:15 +0530

    authentication

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   const addUser = useMutation(api.users.add);
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <p className="text-2xl font-bold">apps / Web</p>
+      <p className="text-2xl font-bold">apps / Widget</p>
       <Button onClick={() => addUser()}>Add</Button>
       <div className="max-w-sm mx-auto w-full">
         {JSON.stringify(users, null, 2)}
